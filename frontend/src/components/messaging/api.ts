364129389f57c0dde2e728d7e5af0e49348dd1a8
textualbois <wivna360@hotmail.com>
2025-12-03 14:45:48 +0100

    comments to messaging/api.ts

--- frontend/src/components/messaging/api.ts
+++ frontend/src/components/messaging/api.ts
@@ -1,9 +1,16 @@
 import type { ConversationId, Conversation, ConversationSummary, Message } from "./types.js";
 
+// fetches lightweight summaries
 export async function fetchConversations(): Promise<ConversationSummary[]> {
-  // fetch("/api/conversations") ...
+  const res = await {}; // fetch('/api/conversations') ...
+  if (!res) {
+	throw new Error("Failed to fetch conversations");
+  }
+  return res as Promise<ConversationSummary[]>; //res.json();
 }
 
+//maybe add scoped fetch later (like fetch the last 20 messages, and fetch more on scroll)
+//fetches full conversation by ID
 export async function fetchConversation(id: ConversationId): Promise<Conversation> {
   const res = await {}; // fetch(`/api/conversations/${id}`) ...
   if (!res) {
@@ -12,6 +19,7 @@ export async function fetchConversation(id: ConversationId): Promise<Conversatio
   return res as Promise<Conversation>; //res.json();
 }
 
+// sends a new message in an existing conversation
 export async function sendMessage(message: Message): Promise<Message> {
 	const res = await {}; // POST to `/api/conversations/${message.conversationId}/messages` ...
 	if (!res) {
@@ -20,6 +28,10 @@ export async function sendMessage(message: Message): Promise<Message> {
 	return res as Promise<Message>; //res.json();
 }
 
+// creates a new conversation with initial message
+// not sure if we should create with oi without initial message
+// i though creating without message may cause backend race if users create and send simultaneously
+// also like this we dont have to deal with empty conversations
 export async function createConversation(participantIds: string[], message: Message): Promise<Message> { 
 	const res = await {}; // POST to `/api/conversations` ...
 	if (!res) {
@@ -28,6 +40,8 @@ export async function createConversation(participantIds: string[], message: Mess
 	return res as Promise<Message>; //res.json();
 }
 
+// edit message
+// absolute least importance feature
 export async function editMessage(message: Message): Promise<Message> {
 	const res = await {}; // POST?PUT
 	if (!res) {
