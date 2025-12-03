f7dbadf165fbf605034a3b05c47a7c2fb9622aed
textualbois <wivna360@hotmail.com>
2025-12-03 14:01:08 +0100

    added common and messaging types

---
+++ frontend/src/common/types.ts
@@ -0,0 +1,19 @@
+export type UserId = string;
+
+// data that is allowed to be passed to other usersq
+export interface UserDataPublic {
+  id: UserId;
+  displayName: string;   // shown in UI
+}
+
+// What *only the server / this user* sees.
+export interface UserDataPrivate {
+  // Stuff that should NEVER leak to other users
+  email: string;
+  firstName: string;
+  lastName: string;
+  // ...etc.
+
+  // Public profile block embedded here:
+  public: UserDataPublic;
+}
