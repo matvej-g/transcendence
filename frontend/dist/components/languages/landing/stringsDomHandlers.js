3182ae660db51af446a5fdcd7b6c851b440323b1
Mert5558 <merdal@student.42heilbronn.de>
2026-01-10 21:24:34 +0100

    Remove dist folder from repository (build artifacts)

--- frontend/dist/components/languages/landing/stringsDomHandlers.js
+++
@@ -1,8 +0,0 @@
-import { registerNamespace, applyI18nToDom, onLangChange } from "../i18n.js";
-import { LandingDomStrings } from "./stringsDom.js";
-const LANDING_DOM_NAMESPACE = "landingDom";
-export function initLandingDomI18n() {
-    registerNamespace(LANDING_DOM_NAMESPACE, LandingDomStrings);
-    applyI18nToDom();
-    onLangChange(() => applyI18nToDom());
-}
