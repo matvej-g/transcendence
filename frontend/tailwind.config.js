496b4c414027bbee5dd3856538c629d0104230bd
Matthias Gering <mgering@3-F-10.42heilbronn.de>
2026-01-23 22:05:30 +0100

    removed tailwind CSs CDN

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.js",
+    "./dist/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
