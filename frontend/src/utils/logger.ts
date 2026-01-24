708236a57f0a17544f8e634b61a9fdac32614cca
textualbois <wivna360@hotmail.com>
2026-01-24 20:58:58 +0100

    logger on

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -1,6 +1,6 @@
 // Simple logger utility for controlling console output
 // Set ENABLE_LOGS to false before submission to hide debug logs
-const ENABLE_LOGS = false; // Set to false before submission
+const ENABLE_LOGS = true; // Set to false before submission
 
 export const logger = {
   log: (...args: any[]) => ENABLE_LOGS && console.log(...args),
